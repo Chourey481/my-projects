8f48df7f4ce5fe357f570462e6224f1b772d2624
Chourey481 <yashchourey481@gmail.com>
2024-08-31 13:23:57 +0530

    update

--- source/execptions.py
+++ source/execptions.py
@@ -11,17 +11,10 @@ def error_message_detail(error,error_detail:sys):
 
 class CustomException(Exception):  
     def __init__(self, error_message, error_detail:sys):
-        super.__init__(error_message)
+        super().__init__(error_message)
         self.error_message = error_message_detail(error_message,error_detail=error_detail)
 
     def __str__(self):
         return self.error_message
     
-if __name__=="__main__":
-    try:
-        a=1/10
-    except Exception as e:
-
-        logging.info("divide by zero") 
-        raise CustomException(e,sys)
                
