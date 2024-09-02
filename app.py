d00027aed9ab73cf81d691ef71f18b1650fdb959
Chourey481 <yashchourey481@gmail.com>
2024-09-02 13:01:22 +0530

    data transformation

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from source.logger import logging
 from source.execptions import CustomException
 from source.components.data_ingestion import DataIngestion
 from source.components.data_ingestion import DataIngestionConfig
-# from source.components.data_transformation import DataTransformationConfig,DataTransformation
+from source.components.data_transformation import DataTransformationConfig,DataTransformation
 # from source.components.model_trainer import ModelTrainerConfig,ModelTrainer
 
 import sys
@@ -16,9 +16,9 @@ if __name__=="__main__":
         data_ingestion=DataIngestion()
         train_data_path,test_data_path=data_ingestion.initiate_data_ingestion()
 
-        #data_transformation_config=DataTransformationConfig()
-        # data_transformation=DataTransformation()
-        # train_arr,test_arr,_=data_transformation.initiate_data_transormation(train_data_path,test_data_path)
+        data_transformation_config=DataTransformationConfig()
+        data_transformation=DataTransformation()
+        train_arr,test_arr,_=data_transformation.initiate_data_transormation(train_data_path,test_data_path)
 
         ## Model Training
 
